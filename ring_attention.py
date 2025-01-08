96352e19d93bf281b87c643895afcb60c678d511
Fu-Chiang Chang <fuchiang137@gmail.com>
2025-01-08 00:35:56 +0000

    Added torch profiler utility to attention benchmarks

--- ring_attention.py
+++ ring_attention.py
@@ -5,6 +5,28 @@ from torch import nn
 import torch.distributed as dist
 from transformers import LlamaConfig
 from attention import LlamaAttention, RingLlamaAttention
+from torch.profiler import profile, record_function, ProfilerActivity
+
+
+def init_prof(use_profiler, warmup_iters=2, inf_iters=3):
+    activities = []
+    # activities.append(torch.profiler.ProfilerActivity.CPU)
+    activities.append(torch.profiler.ProfilerActivity.CUDA)
+
+    from contextlib import nullcontext
+
+    ctx = (
+        torch.profiler.profile(
+            activities=activities,
+            schedule=torch.profiler.schedule(wait=0, warmup=warmup_iters, active=inf_iters, repeat=1),
+            on_trace_ready=torch.profiler.tensorboard_trace_handler("./profile/"),
+            record_shapes=True,
+            with_stack=True,
+        )
+        if use_profiler
+        else nullcontext()
+    )
+    return ctx
 
 
 def init_attn_weights(attn_module):
@@ -26,6 +48,11 @@ def main():
     torch.set_default_device(device)
     torch.set_default_dtype(torch.float16)
 
+    use_profiler = True
+
+    num_warmup_iterations = 2
+    num_inf_iterations = 3
+
     # Configuration
     cfg = LlamaConfig()
     cfg.hidden_size = 4096
@@ -38,85 +65,36 @@ def main():
     cfg._attn_implementation = "sdpa"
     cfg.torch_dtype = torch.float16
 
+    parallel_attn = RingLlamaAttention(cfg).to(device)
+    init_attn_weights(parallel_attn)
+
     # Example input and configuration
     batch_size = 1
-    for seq_len in [4096]:
-        chunk_len = seq_len // world_size
+    seq_len = 4096
+    with torch.no_grad():
+        ctx = init_prof(use_profiler, num_warmup_iterations, num_inf_iterations)
+        with ctx as prof:
+            for _ in range(num_warmup_iterations + num_inf_iterations):
+                chunk_len = seq_len // world_size
 
-        input_tensor = torch.zeros([batch_size, chunk_len, cfg.hidden_size])
-        nn.init.xavier_normal_(input_tensor)
-        position_ids = (
-            torch.arange(chunk_len).unsqueeze(0).expand(input_tensor.shape[0], -1)
-        )
-        position_ids += rank * chunk_len
+                input_tensor = torch.zeros([batch_size, chunk_len, cfg.hidden_size])
+                nn.init.xavier_normal_(input_tensor)
+                position_ids = (
+                    torch.arange(chunk_len)
+                    .unsqueeze(0)
+                    .expand(input_tensor.shape[0], -1)
+                )
+                position_ids += rank * chunk_len
 
-        # # ensure every rank has the same input tensor
-        # dist.broadcast(input_tensor, src=0)
+                # ensure every rank has the same input tensor
+                dist.broadcast(input_tensor, src=0)
 
-        # # Instantiate the parallel attn and local attn
-        # local_attn = LlamaAttention(cfg).to(device)
-        # init_attn_weights(local_attn)
-
-        parallel_attn = RingLlamaAttention(cfg).to(device)
-        init_attn_weights(parallel_attn)
-
-        # warmup
-        num_warmup_runs = 10
-        for _ in range(num_warmup_runs):
-            with torch.no_grad():
-                # local_output, _, _ = local_attn(input_tensor, position_ids=position_ids)
                 parallel_output, _, _ = parallel_attn(
                     input_tensor, position_ids=position_ids
                 )
-        dist.barrier()
 
-        # Forward pass on GPU
-        num_runs = 20
-        parallel_time = 0.0
-        for _ in range(num_runs):
-            dist.barrier()
-            with torch.no_grad():
-                torch.cuda.synchronize()
-                start_time = time.time()
-                parallel_output, _, _ = parallel_attn(
-                    input_tensor, position_ids=position_ids
-                )
-                torch.cuda.synchronize()
-                end_time = time.time()
-
-                parallel_time += end_time - start_time
-        parallel_time /= num_runs
-        print(f"Rank {rank}: time for parallel attention: {parallel_time * 1000:.3} ms")
-
-        # Reduce the maximum parallel time to rank 0
-        parallel_time_tensor = torch.tensor(parallel_time, device=device)
-        dist.reduce(parallel_time_tensor, dst=0, op=dist.ReduceOp.MAX)
-        if rank == 0:
-            max_parallel_time = parallel_time_tensor.item()
-            print(f"Max parallel attention time: {max_parallel_time * 1000:.3} ms")
-
-        # if rank == 0:
-        #     with torch.no_grad():
-        #         torch.cuda.synchronize()
-        #         start_time = time.time()
-        #         local_output, _, _ = local_attn(input_tensor, position_ids=position_ids)
-        #         torch.cuda.synchronize()
-        #         end_time = time.time()
-
-        #         local_time = end_time - start_time
-        #         print(f"time for local attention: {local_time * 1000:.3} ms")
-
-        # Verification: Check if the outputs are close
-        # if rank == 0:
-        #     if torch.allclose(parallel_output, local_output, atol=1e-2):
-        #         print(
-        #             f"Rank {rank}: Verification passed: Parallel and local Attention outputs are close."
-        #         )
-        #     else:
-        #         print(f"Rank {rank}: Verification failed: Outputs differ significantly.")
-
-        # print(f"Rank {rank} Local Output:", local_output)
-        # print(f"Rank {rank} Parallel Output:", parallel_output)
+                if use_profiler:
+                    prof.step()
 
     dist.barrier()
     dist.destroy_process_group()
